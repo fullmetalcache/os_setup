1daf7e2c3ece9c4eb9e2c2a69906eca958e32b32
fmc <fullmetalcache@gmail.com>
2018-04-13 20:15:11 +0000

    Added Java 8 Install

--- debian_9.sh
+++ debian_9.sh
@@ -3,7 +3,14 @@
 apt-get update;
 apt-get upgrade -y;
 
-apt-get install -y curl python-dev python-pip git nikto nmap golang libunwind8 gettext apt-transport-https apache2 dnsutils telnet proxychains zip p7zip libffi-dev libssl-dev;
+apt-get install -y curl python-dev python-pip git nikto nmap golang libunwind8 gettext apt-transport-https apache2 dnsutils telnet proxychains zip p7zip libffi-dev libssl-dev dirmngr;
+
+#Install Java 8
+echo 'deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /etc/apt/sources.list.d/java-8-debian.list;
+echo 'deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main' >> /etc/apt/sources.list.d/java-8-debian.list;
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886;
+apt-get update;
+apt-get install oracle-java8-installer;
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
 mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg;
